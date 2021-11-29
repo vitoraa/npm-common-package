7b90a465b52aa881f8dc987bbc1008241ddfaa58
vitoraa <vitor.lima@outlook.com>
2021-11-29 17:57:31 -0300

    return function

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -15,7 +15,7 @@ declare global {
 }
 
 export const currentUser = (jwtKey: string) => {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return function (req: Request, res: Response, next: NextFunction) {
     if (!req.session?.jwt) {
       return next();
     }
