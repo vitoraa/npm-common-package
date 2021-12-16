f35fe8a16058ce06a5bb8bb61f24b1434d0f476e
vitoraa <vitor.lima@outlook.com>
2021-12-16 16:07:43 -0300

    order created and order cancelled events

---
+++ src/events/order-created-event.ts
@@ -0,0 +1,16 @@
+import { OrderStatus } from "..";
+import { Subjects } from "./subjects";
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    status: OrderStatus
+    ticket: {
+      id: string;
+      price: number
+    };
+    userId: string;
+    expiresAt: string;
+  }
+}
